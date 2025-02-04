127054bbc5aa29c67967b47524af9b0325143db3
chelipika <103322024+chelipika@users.noreply.github.com>
2025-02-04 20:27:45 +0500

    Error handler for keyboardinterrupt is added bot.py

--- bot.py
+++ bot.py
@@ -17,5 +17,8 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("exit")
 
