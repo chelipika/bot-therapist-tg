3ec6caf4690116661ffb19e8049ee42d9c9833de
chelipika <pirmatovj15@gmail.com>
2025-02-10 19:43:48 +0500

    voice inline keabords added

--- noor/keyboards.py
+++ noor/keyboards.py
@@ -7,10 +7,23 @@ main = ReplyKeyboardMarkup(keyboard=[
         input_field_placeholder="Write smth...")
 
 settings = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="History", callback_data="history_callback"), InlineKeyboardButton(text="Fund up", callback_data="fundup")],
-    [InlineKeyboardButton(text="Profile", callback_data="profile")]
+    [InlineKeyboardButton(text="History", callback_data="history_callback"), InlineKeyboardButton(text="Fund up Messages", callback_data="fundup")],
+    [InlineKeyboardButton(text="Profile", callback_data="profile"), InlineKeyboardButton(text="Audio Fund", callback_data="fund_up_audio")],
+    [InlineKeyboardButton(text="Change audio voice", callback_data="voice_change")]
 ])
 
 back_to_main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Back🔙", callback_data="back")]
+])
+
+history_text = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="send it as file", callback_data="send_as_file_history")],
+    [InlineKeyboardButton(text="Back🔙", callback_data="back")]
+])
+
+
+aviable_voices = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Rachel", callback_data="Rachel_change_voice"), InlineKeyboardButton(text="Domi", callback_data="Domi_change_voice")],
+    [InlineKeyboardButton(text="Joseph", callback_data="Joseph_change_voice"), InlineKeyboardButton(text="Liam", callback_data="Liam_change_voice")],
     [InlineKeyboardButton(text="Back🔙", callback_data="back")]
 ])
