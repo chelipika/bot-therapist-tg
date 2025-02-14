4509d6d7e2d02f7f1b1cec70b3cee950bb0174b3
chelipika <103322024+chelipika@users.noreply.github.com>
2025-02-15 01:00:37 +0500

    channel id and link added config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 TOKEN = ""
 GEMINI_API_KEY= ""
 ELEVENLABS_API_KEY = ""
+CHANNEL_ID = -100123456789  # замените на ID вашего канала оставьте -100 \ replace with your channels id after -100 e.g. -100545646545454541213545464
+CHANNEL_LINK = ""  # ссылка на канал link for cahnnel e.g. https://t.me/...........
